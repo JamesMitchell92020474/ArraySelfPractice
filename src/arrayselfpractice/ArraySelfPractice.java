fd81439d470aa3a32ed1f854b48b467ce6441e30
James Mitchell <jamesmitchelldesign1@gmail.com>
2019-08-29 16:55:52 +1200

    Update1

--- src/arrayselfpractice/ArraySelfPractice.java
+++ src/arrayselfpractice/ArraySelfPractice.java
@@ -5,6 +5,8 @@
  */
 package arrayselfpractice;
 
+import java.util.Arrays;
+
 /**
  *
  * @author 92020474
@@ -13,9 +15,37 @@ public class ArraySelfPractice {
 
     /**
      * @param args the command line arguments
+     * @return 
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    public static char main(String[] args) {
+        
+        //Integer long form
+        int[] arrayOfInt = new int[4];
+        arrayOfInt[0] = 1;
+        arrayOfInt[1] = 5;
+        arrayOfInt[2] = 3;
+        arrayOfInt[3] = 6;
+        
+        System.out.println(arrayOfInt[1]);
+        System.out.println(Arrays.toString(arrayOfInt));
+        
+        //Integer short form
+        int[] arrayOfInt2 = {2, 8, 7, 4, 3, 6};
+        
+        System.out.println(arrayOfInt2[2]);
+        System.out.println(Arrays.toString(arrayOfInt2));
+        
+        //Using for loop to display an array
+        int[] arrayOfInt3 = new int[30];
+        
+        for(int i = 0; i < arrayOfInt3.length; i++) {
+            arrayOfInt3[i] = i + 1;
+            // +1 since we want 1-100 and not 0-99
+        }
+        
+        System.out.println(Arrays.toString(arrayOfInt3));
+        return 0;
+        
+   }
     
 }
