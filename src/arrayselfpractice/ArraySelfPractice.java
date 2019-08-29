addbadcb9e2a7c552871eb1e3833067aaeeaea01
James Mitchell <jamesmitchelldesign1@gmail.com>
2019-08-29 17:04:27 +1200

    Update2

--- src/arrayselfpractice/ArraySelfPractice.java
+++ src/arrayselfpractice/ArraySelfPractice.java
@@ -46,6 +46,15 @@ public class ArraySelfPractice {
         System.out.println(Arrays.toString(arrayOfInt3));
         return 0;
         
+        //Printing the alphabet
+        //97
+//        char[] alpha = new char[26];
+//        for(int i = 0; i < 26; i++) {
+//        alpha[i] = (char)(97 + i);
+//        }
+//        System.out.println(Arrays.toString(alpha));
+        
+        
    }
     
 }
